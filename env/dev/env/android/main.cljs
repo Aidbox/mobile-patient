20bafe53289eff921c7b95ecbec01c28863c3241
bismigalis <bismigalis@gmail.com>
2017-06-13 19:57:09 +0300

    files added by re-natal

--- env/dev/env/android/main.cljs
+++ env/dev/env/android/main.cljs
@@ -15,7 +15,7 @@
   (swap! cnt inc))
 
 (figwheel/watch-and-reload
- :websocket-url "ws://localhost:3449/figwheel-ws"
+ :websocket-url "ws://10.0.2.2:3449/figwheel-ws"
  :heads-up-display false
  :jsload-callback force-reload!)
 
