c55bf55a503cdc5f416e5d324210f43edab6e3ef
bismigalis <bismigalis@gmail.com>
2017-06-13 17:03:49 +0300

    re-natal init

---
+++ env/prod/env/android/main.cljs
@@ -0,0 +1,6 @@
+ (ns env.android.main
+  (:require [mobile-patient.android.core :as core]))
+
+ (core/init)
+
+
---
+++ env/prod/env/ios/main.cljs
@@ -0,0 +1,6 @@
+ (ns env.ios.main
+  (:require [mobile-patient.ios.core :as core]))
+
+ (core/init)
+
+
