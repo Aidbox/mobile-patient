33617c4492c4273f70329d6ea17a4052b9f5fb93
bismigalis <bismigalis@gmail.com>
2017-06-23 13:09:31 +0300

    changed colors according to mockup

--- src/mobile_patient/events.cljs
+++ src/mobile_patient/events.cljs
@@ -51,6 +51,7 @@
           source (map #(r/atom %) @chats)]
       [ui/view
        [ui/list-view {:dataSource (.cloneWithRows ds (clj->js source))
+                      :enableEmptySections true
                       :render-row (fn [row]
                                     (r/create-element
                                      row-comp
--- src/mobile_patient/ui.cljs
+++ src/mobile_patient/ui.cljs
@@ -1,5 +1,6 @@
 (ns mobile-patient.ui
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [mobile-patient.color :as color]))
 
 
 (def ReactNative (js/require "react-native"))
@@ -15,7 +16,7 @@
 (def input (r/adapt-react-class (.-TextInput ReactNative)))
 (def Switch (.-Switch ReactNative))
 (def switch (r/adapt-react-class (.-Switch ReactNative)))
-(def button (r/adapt-react-class (.-Button ReactNative)))
+(def rn-button (r/adapt-react-class (.-Button ReactNative)))
 (def date-picker (r/adapt-react-class (aget react-datepicker "default")))
 (def list-view (r/adapt-react-class (.-ListView ReactNative)))
 (def virtualized-list (r/adapt-react-class (.-VirtualizedList ReactNative)))
@@ -28,7 +29,7 @@
 (def touchable-highlight (r/adapt-react-class (.-TouchableHighlight ReactNative)))
 
 (defn alert [title msg]
-  (.alert (.-Alert ReactNative) title msg))
+  (.alert (.-Alert ReactNative) title msg   #js[#js{:text "Close"}]))
 
 (def Icon (js/require "react-native-vector-icons/MaterialIcons"))
 (def icon (r/adapt-react-class (aget Icon "default")))
@@ -37,3 +38,24 @@
 (def table-view (r/adapt-react-class (.-TableView ReactNativeTableviewSimple)))
 (def section (r/adapt-react-class (.-Section ReactNativeTableviewSimple)))
 (def cell (r/adapt-react-class (.-Cell ReactNativeTableviewSimple)))
+
+(defn button [{:keys [title on-press] :as props}]
+  [touchable-highlight {:style {:background-color color/light-grey
+                                :border-width 1
+                                :border-color color/grey
+                                :padding 10
+                                :margin-top 20
+                                }
+                        :on-press on-press}
+   [text {:style {:color color/grey
+                  :font-weight :bold
+                  :text-align :center}}
+    title]])
+
+(defn link [{:keys [title on-press] :as props}]
+  [touchable-highlight {:style {:padding 10}
+                        :on-press on-press}
+   [text {:style {:color color/grey
+                  ;;:font-weight :bold
+                  :text-align :center}}
+    title]])
