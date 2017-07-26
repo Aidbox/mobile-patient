3252cd800faae1cb650f1f5752f0df3d0c7d30d3
Ilya Beda <ir4y.ix@gmail.com>
2017-07-26 23:54:49 +0700

    Fix vitals screen to work with remote-data

--- src/mobile_patient/screen/vitals.cljs
+++ src/mobile_patient/screen/vitals.cljs
@@ -3,67 +3,8 @@
             [re-frame.core :as rf]
             [mobile-patient.ui :as ui]))
 
-
-(def INTERPRITATION {"N" "Normal"
-                     "H" "High"
-                     "L" "Low"
-                     })
-
-(defn show-interpritation [code]
-  (get INTERPRITATION code))
-
-(defn row-component [{:keys [item index header?] :or {header? false}}
-                     navigation]
-  (let [abnormal? (#{"H" "L"} (:interpretation item))
-        border-color "#fff"
-        cell-style {:flex 2
-                    :border-bottom-width 1
-                    :border-color border-color
-                    :padding 10}
-        text-style {:color "#000"}
-        text-style (if header? (assoc text-style :font-weight :bold) text-style)
-        text-style (if abnormal? (assoc text-style :color "red") text-style)
-        ]
-
-    [ui/view {:style {:border-top-width (if header? 1 0)
-                      :border-color border-color
-                      :flex-direction :row
-                      :background-color (if (odd? index) "#fff" "#eee")
-                      }}
-     ;; first column
-     [ui/view {:style (merge cell-style {:border-right-width 1 :border-color border-color})}
-      [ui/touchable-highlight {:style {}
-                               :on-press #(navigation.navigate "ExampleChart")}
-
-       [ui/text {:style (merge text-style (if header? {} {:text-decoration-line :underline}))}
-        (:title item)]]]
-
-     ;; second column
-     [ui/view {:style (merge cell-style {:border-right-width 1 :border-color border-color})}
-      [ui/text {:style text-style}
-       (:value item)]]
-
-     ;; third column
-     [ui/view {:style cell-style}
-      [ui/text {:style text-style}
-       (if header?
-         (:interpretation item)
-         (show-interpritation (:interpretation item)))]]
-     ]))
-
-(defn header-component []
-  [row-component {:item {:title "Vital Sign" :value "Value" :interpretation "Interpretation"}
-                  :index 1
-                  :header? true}])
-
 (defn VitalsScreen [{:keys [navigation]}]
-  (let [data @(rf/subscribe [:get-observations])]
-    (if (empty? data)
-      [ui/text "No data"]
-      [ui/view {:style {:background-color :white}}
-       [ui/shadow-box
-        [header-component]
-        [ui/flat-list
-         {:data (clj->js data)
-          :render-item (fn [row] (r/as-element [row-component (js->clj row :keywordize-keys true)
-                                                navigation]))}]]])))
+  (ui/show-remote-data
+   @(rf/subscribe [:get-observations])
+   (fn [data]
+     [ui/text (str data)])))
--- src/mobile_patient/subs.cljs
+++ src/mobile_patient/subs.cljs
@@ -70,32 +70,19 @@
         (filter #(= (:resourceType %) "Practitioner"))
         (map :id))))
 
-(defmulti get-observation-data #(:id %))
-
-(defmethod get-observation-data "blood-pressure" [item]
-  (for [i [0 1]]
-    {:key (str (get item :id) i)
-     :title (get-in item [:component i :code :coding 0 :display])
-     :value (str (get-in item [:component i :valueQuantity :value]) " "
-                 (get-in item [:component i :valueQuantity :unit]))
-     :interpretation (get-in item [:component i :interpretation :coding 0 :code])
-     }))
-
-(defmethod get-observation-data :default [item]
-  [{:key (get item :id)
-    :title (get-in item [:code :text])
-    :value (str (get-in item [:valueQuantity :value]) " "
-                (get-in item [:valueQuantity :unit]))
-    :interpretation (get-in item [:interpretation :coding 0 :code])
-    }])
+(defn simplify-observation [data]
+  (map #(str (get-in % [:resource :value :Quantity :value])
+              " "
+              (get-in % [:resource :value :Quantity :code]))
+       data))
 
 (reg-sub
  :get-observations
  (fn [db _]
-   (->> (:observations db)
-        (map get-observation-data)
-        (apply concat)
-        )))
+   (let [remote-data (:observations db)]
+     (if (= (:status remote-data) :succeed)
+       (update-in remote-data [:data] simplify-observation)
+       remote-data))))
 
 (reg-sub
  :get-patient-ref-by-id
